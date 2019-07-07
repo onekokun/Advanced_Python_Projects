5a564022c626715e520596bbb09a6caca4b3d60e
Sameer <52498120+onekokun@users.noreply.github.com>
2019-07-07 15:30:55 +0530

    Update open.py

--- Face _Detector/open.py
+++ Face _Detector/open.py
@@ -1,3 +1,7 @@
+# copy this folder on the desktop and then in the command prompt type 'cd Desktop'(for Linux)/'cd desktop' (for windows), 
+# then type 'cd Face_Detector' and then finally type 'python open.py' to view the face detector for Elon Musk
+
+
 import cv2
 
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
