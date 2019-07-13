dbb15bce99899178c952c6f6396bf2aba469fed0
Sameer <52498120+onekokun@users.noreply.github.com>
2019-07-13 13:55:08 +0530

    Update GDP.py

--- Linear_Regression_Project/GDP.py
+++ Linear_Regression_Project/GDP.py
@@ -1,8 +1,5 @@
 from matplotlib import pyplot as plt
-import numpy as np
 import pandas as pd
-import csv
-from matplotlib import style
 
 read = pd.read_csv('/home/physis/Desktop/Linear_Regression_Project/file.csv')
 year = read['year'].values
